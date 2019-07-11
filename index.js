3887097ee271c86cfe7cad7760c25faf06597d77
Kyesha <franciskyesha1@gmail.com>
2019-07-10 23:23:04 -0400

    new project

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function(){
+ // console.log("Hello World!");
+
+//this is to manipulate de Dom
+function calculate(){
+ let totalBill = document.getElementById("total-bill").value;
+   // console.log(document.getElementById("total-bill").value);
+  let serviceOptions = document.getElementById("service-options").value;
+  // console.log(serviceOptions);
+  let totalParty = document.getElementById("total-party").value;
+  // console.log(totalParty);
+  let totalTip = totalBill * serviceOptions;
+  // console.log(totalTip);
+  let myTip = totalTip / totalParty;
+  let myTipAmount = document.createElement("P");
+  document.getElementById("tip-content-container").appendChild(node);
+  textnode = document.createTextNode("");
+}
+
+let submitButton = document.querySelector('#calculate-button');
+// console.log(submitButton);
+
+submitButton.onclick = function() { calculate(); event.preventDefault(); };
+
+});
